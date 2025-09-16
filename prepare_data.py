3ce7ec2b25622ea991b2dfb98a7c48a191786bd0
namwasinyourheart <namgetsupat5@gmail.com>
2025-09-16 15:22:22 +0700

    src: update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -895,7 +895,7 @@ def prepare_multi_data(exp_args, data_args, model_args, device_args):
             #     force_all_strings(normalize_schema(d, map_batch_size=128))
             #     for d in ds_list
             # ]
-            merged_dataset[split] = concatenate_datasets(new_list)
+            # merged_dataset[split] = concatenate_datasets(new_list)
 
 
 
