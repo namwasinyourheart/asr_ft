fc624d06f53cfd7595494bb9742515af4a9f1df6
namwasinyourheart <namgetsupat5@gmail.com>
2025-09-17 13:47:49 +0700

    src: update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -719,8 +719,8 @@ def prepare_multi_data(exp_args, data_args, model_args, device_args):
         merged_dataset = {}
 
         for i, ds_cfg in enumerate(data_args.datasets):
-            if i == 2:
-                break
+            # if i == 2:
+            #     break
             ds_name = ds_cfg.get("name", "unknown")
             hf_raw_data_dir = ds_cfg.get("hf_raw_data_dir", os.path.join(root_data_dir, "raw", "hf", ds_name))
             dataset_script_path = ds_cfg.get("dataset_script_path")
